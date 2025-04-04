c9f994d38714a4f8a0add4cb6830d3a7bb8ae81f
Ting413 <jhope02180901@gmail.com>
2025-04-04 16:55:09 +0800

    Recreate components folder and Page.js

---
+++ components/Page.js
@@ -0,0 +1,9 @@
+function Page({ title, children }) {
+    return React.createElement("div", { className: "main" }, [
+      React.createElement("h1", {}, title),
+      ...(Array.isArray(children) ? children : [children])
+    ]);
+  }
+  
+  export default Page;
+  
